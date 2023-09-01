e67c594873207ef3466afbc320fd44c918b3f039
Najidnadri <muhd.najid.nadri@gmail.com>
2023-09-01 10:54:12 +0800

    included shop widgets in rendering

--- src/lib/sections/shop/ShopWidgets.svelte
+++ src/lib/sections/shop/ShopWidgets.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 import type { StoreWidget } from "$lib/types/shopWidgets"
-import SideQuote from "./widgets/sideQuote.svelte"
-import SimpleQuote from "./widgets/simpleQuote.svelte";
+import ImageGrid12 from "./widgets/ImageGrid12.svelte"
+import ImageGrid6 from "./widgets/ImageGrid6.svelte"
+import ParallelQuote from "./widgets/ParallelQuote.svelte"
+import TextEditor from "./widgets/TextEditor.svelte"
+import RowSliders from "./widgets/RowSliders.svelte"
+import SideQuote from "./widgets/SideQuote.svelte"
+import SimpleQuote from "./widgets/SimpleQuote.svelte";
 
 export let widgets: StoreWidget[] = [];
 
@@ -13,6 +18,16 @@ export let widgets: StoreWidget[] = [];
 			<SimpleQuote props={widget} />
 		{:else if widget.type === "SideQuote"}
 			<SideQuote props={widget} />
+		{:else if widget.type === "ImageGrid12"}
+			<ImageGrid12 props={widget}/>
+		{:else if widget.type === "ParallelQuote"}
+			<ParallelQuote props={widget} />
+		{:else if widget.type === "ImageGrid6"}
+			<ImageGrid6 props={widget} />
+		{:else if widget.type === "TextEditor"}
+			<TextEditor props={widget} />
+		{:else if widget.type === "RowSliders"}
+			<RowSliders props={widget} />
 		{/if}
 	{/each}
 </div>
